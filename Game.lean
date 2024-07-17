6c37c2cdf6bd4aca273d616612fddef79587f6c2
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2024-07-17 12:19:06 +0100

    test2

--- Game.lean
+++ Game.lean
@@ -4,7 +4,7 @@ import Game.Levels.DemoWorld
 Title "Dungeons and De Morgan's: The logic Game"
 Introduction
 "
-# Dungeons and De Morgan's: The logic Game
+
 
 ## By Chris Birkbeck and Niki Kalaydzhieva
 
@@ -22,12 +22,7 @@ Have fun!
 "
 
 Info "
-# Credits
 
-Some levels have been shamelessly borrowed from Kevin Buzzard's and Mohammad Pedramfar's
-<a href="https://www.ma.imperial.ac.uk/~buzzard/xena/natural_number_game/" target="blank">Natural Numbers Game</a>
-as well  as Kevin Buzzard's <a href="https://www.ma.imperial.ac.uk/~buzzard/xena/formalising-mathematics-2022/" target="blank">Formalising mathematics</a>
-course at Imperial College London.
 "
 
 /-! Information to be displayed on the servers landing page. -/
