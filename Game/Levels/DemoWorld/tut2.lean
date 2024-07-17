e44f0748404f6766a1641dc4c7fc36d774f76fe4
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2024-07-17 13:29:32 +0100

    test4

---
+++ Game/Levels/DemoWorld/tut2.lean
@@ -0,0 +1,27 @@
+import Game.Metadata
+
+World "Tutorial"
+Level 2
+
+Title "Tutorial World 2"
+
+Introduction
+"
+
+
+"
+
+
+/-Lemma
+If $P,Q$ are logical statements with respective proofs $p,q$, then $Q$ is true.
+-/
+statement example_two (P Q : Prop) (p : P) (q : Q) : Q := by
+  exact q
+
+Conclusion "Yay!"
+
+/- Use these commands to add items to the game's inventory. -/
+
+NewTactic exact
+-- NewTheorem Nat.add_comm Nat.add_assoc
+-- NewDefinition Nat Add Eq
